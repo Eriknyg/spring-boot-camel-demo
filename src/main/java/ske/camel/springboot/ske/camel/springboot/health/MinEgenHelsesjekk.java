b709427ad3fb2fdbed68f9a25a2f09dccf03a30a
Erik Nygren <erik.nygren@online.no>
2015-11-21 21:07:24 +0100

    Muliggjør overstyring av helse

--- src/main/java/ske/camel/springboot/ske/camel/springboot/health/MinEgenHelsesjekk.java
+++ src/main/java/ske/camel/springboot/ske/camel/springboot/health/MinEgenHelsesjekk.java
@@ -1,8 +1,13 @@
 package ske.camel.springboot.ske.camel.springboot.health;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
+
+
 /**
  * Created by m86922 on 20.11.15.
  */
@@ -10,12 +15,20 @@ import org.springframework.stereotype.Component;
 public class MinEgenHelsesjekk implements HealthIndicator {
 
         private static final String WARN = "WARN";
+        private static Logger LOGGER = LoggerFactory.getLogger(MinEgenHelsesjekk.class);
+
+
+        @Value("${health}")
+        private String health;
+
         @Override
         public Health health() {
-            String errorCode = "{{health}}";
+
+            LOGGER.info("Helsen er: "+health);
+
             // perform some specific health check
-            if (WARN.equals(errorCode)) {
-                return Health.unknown().withDetail("Error code", errorCode).build();
+            if (WARN.equals(health)) {
+                return Health.outOfService().withDetail("Error code", WARN).build();
             }
             return Health.up().build();
         }
